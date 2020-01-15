78593fa8dc88cc47e029e0a050a23d26a9c2e0ef
Gil <ghaerdi@ipisdosaj.edu.do>
2020-01-14 21:46:59 -0400

    I think the hide BTN is working now

--- Primera pagina/close_responsive_media_menu.js
+++ Primera pagina/close_responsive_media_menu.js
@@ -1,7 +1,11 @@
 let menu = document.getElementsByClassName("menu")[0]
-
+let hideBTN = document.getElementsByClassName("hide-menu-btn")[0]
 let chkBox = document.getElementById("chk")
 
 menu.onclick = function() {
+        chkBox.checked = false
+}
+
+hideBTN.onclick = function() {
         chkBox.checked = false
 }
